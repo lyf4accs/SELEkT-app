865dfddd60d720e4871e97ce1ba5ff57cc11bbd1
Laura Junli Illan Ferrandez <LJILLFER@upv.edu.es>
2025-02-28 21:20:25 +0100

    rollback

--- SELEkT-app/src/app/app.routes.ts
+++ SELEkT-app/src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { DropsendComponent } from './dropsend/dropsend.component';
 import { AlbumComponent } from './album/album.component';
 import { UploadFilesComponent } from './upload-files/upload-files.component';
 import { LinksComponent } from './links/links.component';
-import { SwiperSelektComponent } from './swiper-selekt/swiper-selekt.component';
+// import { SwiperSelektComponent } from './swiper-selekt/swiper-selekt.component';
 
 export const routes: Routes = [
   { path: 'dropsend', component: DropsendComponent },
   { path: 'album/:code', component: AlbumComponent },
   { path:'upload', component: UploadFilesComponent },
-  { path:'links', component:LinksComponent },
-  { path: 'swiper', component:SwiperSelektComponent },
+   { path:'links', component:LinksComponent },
+  // { path: 'swiper', component:SwiperSelektComponent },
 ]
--- SELEkT-app/src/app/swiper-selekt/swiper-selekt.component.spec.ts
+++ SELEkT-app/src/app/swiper-selekt/swiper-selekt.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SwiperSelektComponent } from './swiper-selekt.component';
+// // import { SwiperSelektComponent } from './swiper-selekt.component';
 
-describe('SwiperSelektComponent', () => {
-  let component: SwiperSelektComponent;
-  let fixture: ComponentFixture<SwiperSelektComponent>;
+// describe('SwiperSelektComponent', () => {
+//   let component: SwiperSelektComponent;
+//   let fixture: ComponentFixture<SwiperSelektComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SwiperSelektComponent]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [SwiperSelektComponent]
+//     })
+//     .compileComponents();
 
-    fixture = TestBed.createComponent(SwiperSelektComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(SwiperSelektComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
