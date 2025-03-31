006a75cfacfce5ed5a078f85a766e83bad0badfe
JaviLatorre <javierlatorrecano@gmail.com>
2025-03-31 19:53:33 +0200

    change password empezando

--- SELEkT-app/src/app/profile/profile.component.ts
+++ SELEkT-app/src/app/profile/profile.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { PhotoLibraryService } from '../services/PhotoLibraryService';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from "../footer/footer.component";  // Asegúrate de importar FormsModule
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -11,77 +11,27 @@ import { FooterComponent } from "../footer/footer.component";  // Asegúrate de 
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent implements OnInit {
-  moodboards: any[] = []; // Almacenar los moodboards
-  newMoodboardName: string = ''; // Nombre del nuevo moodboard
-  selectedMoodboard?: any; // Para almacenar el moodboard seleccionado
-  imagesForMoodboard: string[] = []; // Imágenes seleccionadas para un moodboard
+export class ProfileComponent {
+  constructor(private router: Router) {}
 
-  photoService = inject(PhotoLibraryService);
+  user = {
+    username: '@juanperez',
+    email: 'juan@example.com',
+  };
 
-  ngOnInit() {
-    // Obtener los moodboards del backend
-    this.loadMoodboards();
+  goToPremium() {
+    this.router.navigate(['/upload']);
   }
 
-  // Método para cargar los moodboards desde el backend
-  loadMoodboards() {
-    // this.photoService.getMoodboards().subscribe(
-    //   (data) => {
-    //     this.moodboards = data.moodboards;
-    //     console.log('Moodboards cargados:', this.moodboards);
-    //   },
-    //   (error) => {
-    //     console.error('Error al cargar los moodboards:', error);
-    //   }
-    // );
+  goToConfig() {
+    this.router.navigate(['/upload']);
   }
 
-  // Método para crear un nuevo moodboard
-  createMoodboard() {
-    // if (this.newMoodboardName) {
-    //   this.photoService
-    //     .createMoodboard({ name: this.newMoodboardName })
-    //     .subscribe(
-    //       (data) => {
-    //         this.moodboards.push(data.moodboard);
-    //         this.newMoodboardName = ''; // Limpiar el campo de nombre
-    //         console.log('Nuevo moodboard creado:', data.moodboard);
-    //       },
-    //       (error) => {
-    //         console.error('Error al crear el moodboard:', error);
-    //       }
-    //     );
-    // }
+  goToColores() {
+    this.router.navigate(['/upload']);
   }
 
-  // Método para añadir imágenes a un moodboard
-  addImagesToMoodboard() {
-    // if (this.selectedMoodboard && this.imagesForMoodboard.length > 0) {
-    //   this.photoService
-    //     .addImagesToMoodboard(
-    //       this.selectedMoodboard.id,
-    //       this.imagesForMoodboard
-    //     )
-    //     .subscribe(
-    //       (data) => {
-    //         console.log('Imágenes añadidas al moodboard:', data);
-    //       },
-    //       (error) => {
-    //         console.error('Error al añadir imágenes al moodboard:', error);
-    //       }
-    //     );
-    // }
-  }
-
-  // Método para seleccionar un moodboard
-  selectMoodboard(moodboard: any) {
-    this.selectedMoodboard = moodboard;
-    this.imagesForMoodboard = [];
-  }
-
-  // Método para añadir imágenes seleccionadas al moodboard
-  selectImagesForMoodboard(images: string[]) {
-    this.imagesForMoodboard = images;
+  goToChange() {
+    this.router.navigate(['/changePassword']);
   }
 }
