952f01a6058395ce2b8b0aa43e17838972e367c6
Laura Junli Illan Ferrandez <LJILLFER@upv.edu.es>
2025-04-05 22:24:36 +0200

    url correction

--- SELEkT-app/src/app/services/PhotoLibraryService.ts
+++ SELEkT-app/src/app/services/PhotoLibraryService.ts
@@ -7,8 +7,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class PhotoLibraryService {
-  private apiUrl =
-    environment.apiUrl;
+  private apiUrl = environment.apiUrl + '/api/processImages';
 
   constructor(private http: HttpClient) {}
 
