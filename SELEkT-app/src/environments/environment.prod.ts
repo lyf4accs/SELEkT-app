e5df735ef82b674f39bbc517205dd5fcedeec8e2
Laura Junli Illan Ferrandez <LJILLFER@upv.edu.es>
2025-04-08 16:44:46 +0200

    vercel version

--- SELEkT-app/src/environments/environment.prod.ts
+++ SELEkT-app/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://selekt-server.onrender.com',
+  apiUrl: 'https://selekt-server.vercel.app',
 };
-//https://selekt-server.vercel.app
