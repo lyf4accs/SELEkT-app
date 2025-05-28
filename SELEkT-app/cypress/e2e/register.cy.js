8eb5ef9b95afe2294fb641b01960d9f05c2336fb
JaviLatorre <162303283+JaviLatorre@users.noreply.github.com>
2025-05-28 21:23:38 +0200

    ca

--- SELEkT-app/cypress/e2e/register.cy.js
+++ SELEkT-app/cypress/e2e/register.cy.js
@@ -4,10 +4,12 @@ describe("Registro de usuario", () => {
   });
 
   it("Muestra error si faltan campos", () => {
-    // Solo rellena un campo
-    cy.get("#correo").type("javi@example.com");
+    // No se rellena nada
     cy.get(".submit-button").click();
-    cy.get(".error-message").should("contain", "Rellena todos los campos");
+    cy.get(".error-message", { timeout: 4000 }).should(
+      "contain",
+      "Rellena todos los campos"
+    );
   });
 
   it("Muestra error si el nombre tiene números", () => {
@@ -22,9 +24,9 @@ describe("Registro de usuario", () => {
     );
   });
 
-  it("Muestra error si los emails son inválidos", () => {
+  it("Muestra error si el correo es inválido", () => {
     cy.get("#nombre").type("Javier");
-    cy.get("#correo").type("javi@correo"); // inválido
+    cy.get("#correo").type("correo@invalido"); // sin dominio
     cy.get("#password").type("password123");
     cy.get("#rep").type("password123");
     cy.get(".submit-button").click();
@@ -38,17 +40,16 @@ describe("Registro de usuario", () => {
     cy.get("#nombre").type("Javier");
     cy.get("#correo").type("javi@example.com");
     cy.get("#password").type("password123");
-    cy.get("#rep").type("diferente123");
+    cy.get("#rep").type("otra1234");
     cy.get(".submit-button").click();
     cy.get(".error-message").should("contain", "Las contraseñas no coinciden");
   });
 
-  it("Registra con éxito con datos válidos", () => {
+  it("Muestra error si el correo ya está registrado", () => {
     cy.intercept("POST", "**/auth/v1/signup", {
-      statusCode: 200,
+      statusCode: 400,
       body: {
-        user: { email: "javi@example.com" },
-        session: null,
+        error: "Correo a registrado",
       },
     }).as("signUp");
 
@@ -56,8 +57,32 @@ describe("Registro de usuario", () => {
     cy.get("#correo").type("javi@example.com");
     cy.get("#password").type("password123");
     cy.get("#rep").type("password123");
-
     cy.get(".submit-button").click();
+
+    cy.wait("@signUp");
+    cy.get(".error-message", { timeout: 4000 }).should(
+      "contain",
+      "Correo a registrado"
+    );
+  });
+
+  it("Registra con éxito con datos válidos", () => {
+    const email = `javi${Date.now()}@example.com`;
+
+    cy.intercept("POST", "**/auth/v1/signup", {
+      statusCode: 200,
+      body: {
+        user: { email },
+        session: null,
+      },
+    }).as("signUp");
+
+    cy.get("#nombre").type("Javier");
+    cy.get("#correo").type(email);
+    cy.get("#password").type("password123");
+    cy.get("#rep").type("password123");
+    cy.get(".submit-button").click();
+
     cy.wait("@signUp");
     cy.get(".error-message").should("not.exist");
   });
